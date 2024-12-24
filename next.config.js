fc5539da2b6bea6af78c13b163a79ab360c868bc
iran <iransaboonian@gmail.com>
2024-12-24 08:15:20 +0330

    remove output:export from next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-// const isProd = process.env.NODE_ENV ===  "production";
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true},
-  // output: 'export'
+  images: { unoptimized: true}
 }
 
 // export default nextConfig;
